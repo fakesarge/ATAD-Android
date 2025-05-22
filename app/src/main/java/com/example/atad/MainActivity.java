d7379cc91310af463c7db6fd0226f6ca4fa1e630
sargeff <sargentxtrad@gmail.com>
2025-05-21 21:38:37 -0400

    Initial Application

---
+++ app/src/main/java/com/example/atad/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.atad;
+import android.content.Intent;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        auth = FirebaseAuth.getInstance();
+
+        // If the user is already logged in this wont happen. If the current user is null it will redirect.
+        if (auth.getCurrentUser() == null){
+            Intent intent = new Intent(MainActivity.this, login.class);
+            startActivity(intent);
+        }
+
+
+    }
+}
