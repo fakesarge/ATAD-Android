e1336333d37ae9065d4b220e0abab561293cf83f
sargeff <sargentxtrad@gmail.com>
2025-05-24 20:58:18 -0400

    5grsdgg

---
+++ app/src/main/java/com/example/atad/Users.java
@@ -0,0 +1,40 @@
+package com.example.atad;
+
+public class Users {
+    String username, password, email, userID;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public Users(){}
+}
--- app/src/main/java/com/example/atad/signup.java
+++ app/src/main/java/com/example/atad/signup.java
@@ -1,19 +1,33 @@
 package com.example.atad;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
 public class signup extends AppCompatActivity {
 
     ImageButton signup;
     EditText rg_password, rg_repassword, rg_email, rg_username;
+    FirebaseAuth auth;
+
+
 
 
     @Override
@@ -26,5 +40,48 @@ public class signup extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        signup = findViewById(R.id.signup);
+        rg_username = findViewById(R.id.username);
+        rg_email = findViewById(R.id.email);
+        rg_password = findViewById(R.id.password);
+        rg_repassword = findViewById(R.id.repassword);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = rg_username.getText().toString();
+                String email = rg_email.getText().toString();
+                String password = rg_password.getText().toString();
+                String repassword = rg_password.getText().toString();
+
+                if (TextUtils.isEmpty(name) || TextUtils.isEmpty(email) || TextUtils.isEmpty(password) || TextUtils.isEmpty(repassword))
+                {
+                    Toast.makeText(signup.this, "Please Enter Valid Information", Toast.LENGTH_SHORT).show();
+
+                } else if(!password.equals(repassword)){
+                    Toast.makeText(signup.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                } else
+                {
+                    auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful())
+                            {
+                                String id = task.getResult().getUser().getUid();
+//                                DatabaseReference reference = database.get
+                            }
+                        }
+                    });
+                }
+
+            }
+        });
+
+
+
+
     }
+
+
 }
