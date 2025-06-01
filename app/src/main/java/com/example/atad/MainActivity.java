8d3a976fdca81b2730511d297876420755ee040d
fakesarge <sargentxtrad@gmail.com>
2025-06-01 18:56:02 -0400

    m

--- app/src/main/java/com/example/atad/MainActivity.java
+++ app/src/main/java/com/example/atad/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -23,7 +24,10 @@ import com.google.firebase.auth.FirebaseAuth;
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
-    ImageButton open_password_gen;
+    ImageButton open_password_gen, open_breach_search, open_password_storage;
+
+    LeakAPI api = new LeakAPI();
+    Users user = new Users();
 
 
     @Override
@@ -31,14 +35,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+//        try {
+//            if(api.search("password123",false))
+//            {
+//                Toast.makeText(MainActivity.this,
+//                        "Your Password has been breached please change it.",
+//                        Toast.LENGTH_SHORT).show();
+//            }
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+
         auth = FirebaseAuth.getInstance();
         open_password_gen = findViewById(R.id.openPassword);
+        open_breach_search = findViewById(R.id.openDarkWeb);
+        open_password_storage = findViewById(R.id.openDb);
+
 
 
 
@@ -53,6 +74,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent( MainActivity.this, PasswordGenActivity.class);
+                startActivity(intent);
+            }
+        });
+        open_password_storage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, PasswordStorageActivity.class);
+                startActivity(intent);
+            }
+        });
+        open_breach_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, PasswordSearchActivity.class);
                 startActivity(intent);
             }
         });
